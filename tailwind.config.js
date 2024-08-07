1e3675742a0e679a4736fc3474cf8878d62118b6
AMAN KUMAR CHHARI <140314215+chhari07@users.noreply.github.com>
2024-08-08 00:22:35 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,15 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
+  colors: {
+    background: '#060608',
+    color: '#FAFAFA',
+  },
+  fontSize: {
+    '1.5rem': '1.5rem',
+  },
+  gridTemplateColumns: {
+    '1fr': '1fr',
+  },
 }
 
